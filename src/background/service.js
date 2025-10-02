64c7061b021dee64f5cd58844a0fc4bac66196bc
hababr <hhababr@gmail.com>
2025-10-02 15:51:22 +0300

    fix: copy cacheSieve func from v0.9.8.74 and fix the bug with sieveResLocal not cashed correctly

--- src/background/service.js
+++ src/background/service.js
@@ -1,4 +1,8 @@
 "use strict";
+
+var cachedSieve = [],
+    sieveResLocal = [];
+
 var cfg = {
     sessionGet: (e, r) => (r ? chrome.storage.session.get(e, r) : chrome.storage.session.get(e)),
     sessionSet: (e) => chrome.storage.session.set(e),
@@ -71,34 +75,39 @@ const withBaseURI = function (e, r, s) {
                     });
         });
     },
-    cacheSieve = function (e) {
-        e = "string" == typeof e ? JSON.parse(e) : JSON.parse(JSON.stringify(e));
-        var r = [],
-            s = [];
-        for (var t in e) {
-            var a = e[t];
-            if ((a.link || a.img) && (!a.img || a.to || a.res)) {
-                try {
-                    if (a.off) throw t + " is off";
-                    if (a.res)
-                        if (/^:\n/.test(a.res)) (s[r.length] = a.res.slice(2)), (a.res = 1);
-                        else {
-                            if (a.res.indexOf("\n") > -1) {
-                                var o = a.res.split(/\n+/);
-                                (a.res = RegExp(o[0])), o[1] && (a.res = [a.res, RegExp(o[1])]);
-                            } else a.res = RegExp(a.res);
-                            (s[r.length] = a.res), (a.res = !0);
-                        }
-                } catch (e) {
-                    "object" == typeof e ? console.error(t, a, e) : console.info(e);
-                    continue;
-                }
-                a.to && a.to.indexOf("\n") > 0 && 0 !== a.to.indexOf(":\n") && (a.to = a.to.split("\n")), delete a.note, r.push(a);
+    cacheSieve = function (newSieve) {
+        if (typeof newSieve === "string") newSieve = JSON.parse(newSieve);
+        else newSieve = JSON.parse(JSON.stringify(newSieve));
+        cachedSieve = [];
+        sieveResLocal = [];
+
+        for (var ruleName in newSieve) {
+            var rule = newSieve[ruleName];
+            if ((!rule.link && !rule.img) || (rule.img && !rule.to && !rule.res)) continue;
+            try {
+                if (rule.off) throw ruleName + " is off";
+                if (rule.res)
+                    if (/^:\n/.test(rule.res)) {
+                        sieveResLocal[cachedSieve.length] = rule.res.slice(2);
+                        rule.res = 1;
+                    } else {
+                        if (rule.res.indexOf("\n") > -1) {
+                            var lines = rule.res.split(/\n+/);
+                            rule.res = RegExp(lines[0]);
+                            if (lines[1]) rule.res = [rule.res, RegExp(lines[1])];
+                        } else rule.res = RegExp(rule.res);
+                        sieveResLocal[cachedSieve.length] = rule.res;
+                        rule.res = true;
+                    }
+            } catch (ex) {
+                if (typeof ex === "object") console.error(ruleName, rule, ex);
+                else console.info(ex);
+                continue;
             }
+            if (rule.to && rule.to.indexOf("\n") > 0 && rule.to.indexOf(":\n") !== 0) rule.to = rule.to.split("\n");
+            delete rule.note;
+            cachedSieve.push(rule);
         }
-        cfg.sessionSet({ cachedSieveRes: s }).then(function () {
-            cfg.sessionSet({ cachedSieve: r });
-        });
     },
     updatePrefs = function (e, r) {
         var s;
@@ -144,12 +153,12 @@ const withBaseURI = function (e, r, s) {
         if ((null === r ? (t = e) : ((a = { msg: e, origin: r.url, postMessage: s }), (t = a.msg)), t.cmd)) {
             switch (t.cmd) {
                 case "hello":
-                    cfg.sessionGet(["cachedPrefs", "cachedSieve"], function ({ cachedPrefs: e, cachedSieve: r }) {
+                    cfg.sessionGet(["cachedPrefs"], function ({ cachedPrefs: e }) {
                         var s,
                             t,
                             o,
                             n = !1,
-                            i = { hz: e.hz, sieve: r, tls: e.tls, keys: e.keys };
+                            i = { hz: e.hz, sieve: cachedSieve, tls: e.tls, keys: e.keys };
                         if (e.grants)
                             for (s = 0, t = (o = e.grants).length; s < t; ++s)
                                 ("*" === o[s].url || (o[s].op[1] && o[s].url.test(a.origin)) || a.origin.indexOf(o[s].url) > -1) && (n = "!" === o[s].op[0]);
@@ -216,74 +225,72 @@ const withBaseURI = function (e, r, s) {
                         });
                     break;
                 case "resolve":
-                    cfg.sessionGet(["cachedSieve", "cachedSieveRes"], function ({ cachedSieve: e, cachedSieveRes: r }) {
-                        var s = { cmd: "resolved", id: t.id, m: null, params: t.params },
-                            o = e[s.params.rule.id];
-                        if ((s.params.rule.req_res && (s.params.rule.req_res = r[s.params.rule.id]), s.params.rule.skip_resolve))
-                            return (s.params.url = [""]), void a.postMessage(s);
-                        var n = /([^\s]+)(?: +:(.+)?)?/.exec(t.url);
-                        (t.url = n[1]),
-                            n[2] || (n[2] = null),
-                            1 === o.res && ((s.m = !0), (s.params._ = ""), (s.params.url = [n[1], n[2]])),
-                            (n = n[2]),
-                            fetch(t.url, { method: n ? "POST" : "GET", body: n, headers: n ? { "Content-Type": "application/x-www-form-urlencoded" } : {} })
-                                .then(function (e) {
-                                    return /^(image|video|audio)\//i.test(e.headers.get("Content-Type"))
-                                        ? ((s.m = t.url),
-                                          (s.noloop = !0),
-                                          console.warn(chrome.runtime.getManifest().name + ": rule " + s.params.rule.id + " matched against an image file"),
-                                          void a.postMessage(s))
-                                        : e.text();
-                                })
-                                .then(function (e) {
-                                    var n = e.slice(0, 4096);
-                                    if (
-                                        ((n = (n = /<base\s+href\s*=\s*("[^"]+"|'[^']+')/.exec(n))
-                                            ? withBaseURI(t.url, n[1].slice(1, -1).replace(/&amp;/g, "&"), !0)
-                                            : t.url),
-                                        1 === o.res)
-                                    )
-                                        return (s.params._ = e), (s.params.base = n.replace(/(\/)[^\/]*(?:[?#].*)*$/, "$1")), void a.postMessage(s);
-                                    var i = r[s.params.rule.id],
-                                        c = (i = (Array.isArray(i) ? i : [i]).map(function (e) {
-                                            var r = e.source || e;
-                                            if (-1 === r.indexOf("$")) return e;
-                                            var t = s.params.length;
-                                            return (
-                                                (t = Array.apply(null, Array(t))
-                                                    .map(function (e, r) {
-                                                        return r;
+                    var s = { cmd: "resolved", id: t.id, m: null, params: t.params },
+                        o = cachedSieve[s.params.rule.id];
+                    if ((s.params.rule.req_res && (s.params.rule.req_res = sieveResLocal[s.params.rule.id]), s.params.rule.skip_resolve))
+                        return (s.params.url = [""]), void a.postMessage(s);
+                    var n = /([^\s]+)(?: +:(.+)?)?/.exec(t.url);
+                    (t.url = n[1]),
+                        n[2] || (n[2] = null),
+                        1 === o.res && ((s.m = !0), (s.params._ = ""), (s.params.url = [n[1], n[2]])),
+                        (n = n[2]),
+                        fetch(t.url, { method: n ? "POST" : "GET", body: n, headers: n ? { "Content-Type": "application/x-www-form-urlencoded" } : {} })
+                            .then(function (e) {
+                                return /^(image|video|audio)\//i.test(e.headers.get("Content-Type"))
+                                    ? ((s.m = t.url),
+                                        (s.noloop = !0),
+                                        console.warn(chrome.runtime.getManifest().name + ": rule " + s.params.rule.id + " matched against an image file"),
+                                        void a.postMessage(s))
+                                    : e.text();
+                            })
+                            .then(function (e) {
+                                var n = e.slice(0, 4096);
+                                if (
+                                    ((n = (n = /<base\s+href\s*=\s*("[^"]+"|'[^']+')/.exec(n))
+                                        ? withBaseURI(t.url, n[1].slice(1, -1).replace(/&amp;/g, "&"), !0)
+                                        : t.url),
+                                    1 === o.res)
+                                )
+                                    return (s.params._ = e), (s.params.base = n.replace(/(\/)[^\/]*(?:[?#].*)*$/, "$1")), void a.postMessage(s);
+                                var i = sieveResLocal[s.params.rule.id],
+                                    c = (i = (Array.isArray(i) ? i : [i]).map(function (e) {
+                                        var r = e.source || e;
+                                        if (-1 === r.indexOf("$")) return e;
+                                        var t = s.params.length;
+                                        return (
+                                            (t = Array.apply(null, Array(t))
+                                                .map(function (e, r) {
+                                                    return r;
+                                                })
+                                                .join("|")),
+                                            (t = (t = RegExp("([^\\\\]?)\\$(" + t + ")", "g")).test(r)
+                                                ? r.replace(t, function (e, r, t) {
+                                                        return t < s.params.length && "\\" !== r
+                                                            ? r + (s.params[t] ? s.params[t].replace(/[/\\^$-.+*?|(){}[\]]/g, "\\$&") : "")
+                                                            : e;
                                                     })
-                                                    .join("|")),
-                                                (t = (t = RegExp("([^\\\\]?)\\$(" + t + ")", "g")).test(r)
-                                                    ? r.replace(t, function (e, r, t) {
-                                                          return t < s.params.length && "\\" !== r
-                                                              ? r + (s.params[t] ? s.params[t].replace(/[/\\^$-.+*?|(){}[\]]/g, "\\$&") : "")
-                                                              : e;
-                                                      })
-                                                    : e),
-                                                "string" == typeof e ? t : RegExp(t)
-                                            );
-                                        }))[0].exec(e);
-                                    if (c) {
-                                        var l = s.params.rule.loop_param;
-                                        o.dc &&
-                                            (("link" === l && 2 !== o.dc) || ("img" === l && o.dc > 1)) &&
-                                            (c[1] = decodeURIComponent(decodeURIComponent(c[1]))),
-                                            (s.m = withBaseURI(n, c[1].replace(/&amp;/g, "&"))),
-                                            ((c[2] && (c = c.slice(1))) || (i[1] && (c = i[1].exec(e)))) &&
-                                                (s.m = [
-                                                    s.m,
-                                                    c
-                                                        .filter(function (e, r) {
-                                                            return !(!r || !e);
-                                                        })
-                                                        .join(" - "),
-                                                ]);
-                                    } else console.info(chrome.runtime.getManifest().name + ": no match for " + s.params.rule.id);
-                                    a.postMessage(s);
-                                });
-                    });
+                                                : e),
+                                            "string" == typeof e ? t : RegExp(t)
+                                        );
+                                    }))[0].exec(e);
+                                if (c) {
+                                    var l = s.params.rule.loop_param;
+                                    o.dc &&
+                                        (("link" === l && 2 !== o.dc) || ("img" === l && o.dc > 1)) &&
+                                        (c[1] = decodeURIComponent(decodeURIComponent(c[1]))),
+                                        (s.m = withBaseURI(n, c[1].replace(/&amp;/g, "&"))),
+                                        ((c[2] && (c = c.slice(1))) || (i[1] && (c = i[1].exec(e)))) &&
+                                            (s.m = [
+                                                s.m,
+                                                c
+                                                    .filter(function (e, r) {
+                                                        return !(!r || !e);
+                                                    })
+                                                    .join(" - "),
+                                            ]);
+                                } else console.info(chrome.runtime.getManifest().name + ": no match for " + s.params.rule.id);
+                                a.postMessage(s);
+                            });
             }
             return !0;
         }
